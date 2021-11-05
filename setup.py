588764137bdbc7b05d84ef8634d2c1981565c5cc
Chronicle Codes <34730578+NOLA-Chronicle@users.noreply.github.com>
2021-11-04 19:46:13 -0500

    added setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+   name='GroupMe - Hello World',
+   version='1.0.0',
+   description='Beginning groupme bot',
+   author='Ryan Johnson',
+   author_email='Chronicle.codes@gmail.com',
+   packages=['GroupMe - Hello World'],  #same as name
+   install_requires=['time'], #external packages as dependencies
+)
