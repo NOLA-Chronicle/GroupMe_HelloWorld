8a98c3220a22c7522dc7ebb0c22e0c10111f389e
Chronicle Codes <34730578+NOLA-Chronicle@users.noreply.github.com>
2021-11-04 19:48:44 -0500

    Changed Name

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 from setuptools import setup
 
 setup(
-   name='GroupMe - Hello World',
+   name='GroupMe_HelloWorld',
    version='1.0.0',
    description='Beginning groupme bot',
    author='Ryan Johnson',
    author_email='Chronicle.codes@gmail.com',
-   packages=['GroupMe - Hello World'],  #same as name
+   packages=['GroupMe_HelloWorld'],  #same as name
    install_requires=['time'], #external packages as dependencies
 )
