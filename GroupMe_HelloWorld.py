7005fc9db8ada21fd61a6f6490fd84d01bafea7f
Chronicle Codes <chronicle.codes@gmail.com>
2021-11-12 18:19:35 -0600

    Added persistent memory and config file

--- GroupMe_HelloWorld.py
+++ GroupMe_HelloWorld.py
@@ -1,6 +1,24 @@
 import time
 import os
+import json
 
-print("Starting Hello World Bot...");
+print("Loading...");
 
-print(os.environ['GroupMePrivKey']);
+with open("config.json", "r") as in_configFile:
+	config = json.load(in_configFile)
+
+#Starting Up
+print(config["StartingMsg"])
+
+#Global Declarations
+__GMPrivKey_API = os.environ['GroupMePrivKey']
+baseURL = "https://api.groupme.com/v3"
+
+print("Ping #" + str(config["counter"]))
+
+config["counter"] = config["counter"] + config["increment"]
+
+
+with open("config.json", "w") as out_configFile:
+	json.dump(config, out_configFile)
+
